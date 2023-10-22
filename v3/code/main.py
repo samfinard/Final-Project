ab79eb56d9c52a5c4bfef3a51f5bcfed43e23884
samfinard <104854051+samfinard@users.noreply.github.com>
2023-10-22 15:00:50 -0400

    updated main

--- v3/code/main.py
+++ v3/code/main.py
@@ -4,6 +4,6 @@ import datetime
 def main():
     start, end = datetime.datetime.strptime(sys.argv[1], "%m/%d/%Y"), datetime.datetime.strptime(sys.argv[2], "%m/%d/%Y")
     print(start, end)
-
+    
 if __name__ == "__main__":
     main()
